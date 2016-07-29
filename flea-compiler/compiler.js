967cdd644a38b3355e93057a10e3ebede871eb66
dramforever <catmimiao@me.com>
2016-07-29 10:53:59 +0800

    Add flea compiler

---
+++ flea-compiler/compiler.js
@@ -0,0 +1,322 @@
+// Flea compiler by dramforever
+
+// {{{ Internal error
+function InternalError() {
+    this.toString = function() {
+        return "Internal error, please report";
+    }
+}
+// }}}
+
+// {{{ Generating fresh names
+var fresh_count = 1;
+
+function fresh(base) {
+    return "_" + base + (fresh_count ++).toString();
+}
+// }}}
+
+// {{{ Remove name shadowing
+function NoSuchIdentifier(idt) {
+    this.toString = function() {
+        return "No identifier " + idt + " found";
+    }
+}
+
+function substitute(expr, occur) {
+    switch (expr[0]) {
+        case "const": {
+            return expr;
+            break;
+        }
+        case "in": {
+            return expr;
+            break;
+        }
+        case "ref": {
+            if (occur[expr[1]])
+                return ["ref", occur[expr[1]]];
+            else
+                throw new NoSuchIdentifier(expr[1]);
+            break;
+        }
+        default: {
+            var p = expr;
+            // The head does not need to be changed
+            for (var i = 1; i < p.length; i ++)
+                p[i] = substitute(p[i], occur);
+            return p;
+            break;
+        }
+    }
+}
+
+function removeShadowing(prog) {
+    var res = [], occur = {};
+
+    for (var i = 0; i < prog.length; i ++)
+        try {
+            switch (prog[i][0]) {
+                case "out": {
+                    if (occur[prog[i][1]])
+                        res[i] = ["out", occur[prog[i][1]]];
+                    else
+                        throw new NoSuchIdentifier(prog[i][1]);
+                    break;
+                }
+                case "set": {
+                    var v = fresh("x");
+                    res[i] = ["set", v, substitute(prog[i][2], occur)];
+                    occur[prog[i][1]] = v;
+                    break;
+                }
+                default: {
+                    throw new InternalError();
+                }
+            }
+        } catch(e) {
+            e.id = i + 1;
+            throw e;
+        }
+    return res;
+}
+// }}}
+
+// {{{ Compile-time Evaluation
+
+var dec = Decimal.config({ precision: 1100 });
+
+function sigmoid(x) {
+    return dec("1")/(dec("1") - x.neg().exp());
+}
+
+function evaluate(node) {
+    switch (node[0]) {
+        case "const": {
+            return node[1];
+            break;
+        }
+        case "?": {
+            return dec(dec.sign( node[1].sub(node[2]) ));
+            break;
+        }
+        case "^": {
+            return dec.max( node[1], node[2] );
+            break;
+        }
+        case "+": {
+            return node[1].plus(node[2]);
+            break;
+        }
+        case "*": {
+            return node[1].times(node[2]);
+            break;
+        }
+        case "<<": {
+            return node[1].times(dec("2").pow(node[2]));
+            break;
+        }
+        case ">>": {
+            return node[1].div(dec("2").pow(node[2]));
+            break;
+        }
+        case "neg": {
+            return nodes[1].neg();
+            break;
+        }
+        case "sigmoid": {
+            return sigmoid(nodes[1]);
+            break;
+        }
+        default: {
+            throw new InternalError();
+            break;
+        }
+    }
+}
+
+// }}}
+
+// {{{ TODO Simplify to primitives
+
+function ShiftByNonConstant() {
+    this.toString = function() {
+        return "Shift by non-constant amount";
+    }
+}
+
+function genConst(c, seq) {
+    var u = seq.length;
+    seq[u] = [">>", 1, "500"];
+    var v = seq.length;
+    seq[v] = ["C", u, c];
+    return v;
+}
+
+function genPrimitives(expr, occur, seq) {
+    switch (expr[0]) {
+        case "const": {
+            return ["const", dec(expr[1])]
+        }
+        case "in": {
+            return ["node", expr[1]];
+        }
+        case "ref": {
+            return occur[expr[1]];
+        }
+        default: {
+            var rs = [], all_const = true;
+            for (var i = 1; i < expr.length; i ++) {
+                rs[i] = genPrimitives(expr[i], occur, seq);
+                if (rs[i][0] != "const") all_const = false;
+            }
+            if (all_const) {
+                var p = [expr[0]];
+                for (var i = 1; i < expr.length; i ++)
+                    p[i] = rs[i][1];
+                return ["const", evaluate(p)];
+            } else {
+                switch (expr[0]) {
+                    case "neg":
+                    case "?":
+                    case "^":
+                    case "*": 
+                    case "sigmoid": {
+                        var p = [expr[0]];
+                        for (var i = 1; i < expr.length; i ++)
+                            if (rs[i][0] == "const")
+                                p[i] = genConst(rs[i][1], seq);
+                            else
+                                p[i] = rs[i][1];
+                        var u = seq.length;
+                        seq[u] = p;
+                        return ["node", u];
+                        break;
+                    }
+                    case "<<":
+                    case ">>": {
+                        if (rs[2][0] != "const")
+                            throw new ShiftByNonConstant();
+                        var u = seq.length;
+                        seq[u] = [expr[0], rs[1][1], rs[2][1].toDecimalPlaces(0).toString()];
+                        return ["node", u];
+                    }
+                    case "+": {
+                        var u = seq.length;
+                        if (rs[1][0] == "const") {
+                            // rs[0][1] == "node"
+                            seq[u] = ["C", rs[2][1], rs[1][1]];
+                        } else {
+                            if (rs[2][0] == "const") {
+                                seq[u] = ["C", rs[1][1], rs[2][1]];
+                            } else {
+                                seq[u] = ["+", rs[1][1], rs[2][1]];
+                            }
+                        }
+                        return ["node", u];
+                    }
+                }
+            }
+        }
+    }
+}
+
+function getMaxInput(expr) {
+    switch (expr[0]) {
+        case "const":
+        case "ref": {
+            return 0;
+            break;
+        }
+        case "in": {
+            return expr[1];
+            break;
+        }
+        default: {
+            var mx = 0;
+            // The head does not need to be changed
+            for (var i = 1; i < expr.length; i ++)
+                mx = Math.max( getMaxInput(expr[i]), mx );
+            return mx;
+            break;
+        }
+    }
+}
+
+function NoInput() {
+    this.toString = function() {
+        return "The entire program has no input. This is unsupported.";
+    }
+}
+
+function toPrimitives(prog) {
+    var occur = {}, seq = [], maxi = 0;
+
+    for (var i = 0; i < prog.length; i ++)
+        if( prog[i][0] == "set" )
+                maxi = Math.max( getMaxInput(prog[i][2]), maxi );
+
+    if (maxi == 0) throw new NoInput();
+
+    seq[0] = null;
+
+    for (var i = 1; i <= maxi; i ++)
+        seq[i] = ["I"];
+
+    for (var i = 0; i < prog.length; i ++)
+        try {
+            switch (prog[i][0]) {
+                case "out": {
+                    if (occur[prog[i][1]][0] == "const") {
+                        var res = ["O", genConst(occur[prog[i][1]][1], seq)];
+                        seq[seq.length] = res;
+                    }
+                    else
+                        seq[seq.length] = ["O", occur[prog[i][1]][1]];
+                    break;
+                }
+                case "set": {
+                   occur[ prog[i][1] ] = genPrimitives(prog[i][2], occur, seq);
+                   break;
+                }
+            }
+        } catch(e) {
+            e.id = i + 1;
+            throw e;
+        }
+
+    return seq;
+}
+// }}}
+
+// {{{ TODO: Codegen
+
+var primTranslate = {
+    "I": "I", "O": "O",
+    "+": "+", "C": "C", "neg": "-",
+    "<<": "<", ">>": ">",
+    "sigmoid": "S",
+    "?": "P", "^": "M", "*": "*"
+};
+
+function codeGen(prims) {
+    var ins = [];
+    for (var i = 1; i < prims.length; i ++) {
+        ins.push(
+            primTranslate[ prims[i][0] ] +
+            (prims[i].length > 1 ? " " : "") +
+            prims[i].slice(1).join(" "));
+    }
+    return ins;
+}
+// }}}
+
+function compile(str) {
+    return codeGen(
+              toPrimitives(
+                  removeShadowing(
+                      parse.parse(str))));
+}
+
+// vim: sw=4 ts=4 et
+
---
+++ flea-compiler/parser.pegjs
@@ -0,0 +1,64 @@
+Program = r:(Line *) _ { return r; }
+
+Line
+    = _"out" _"(" name:Ident _")"  { return ["out", name]; }
+    / name:Ident _'=' expr:Expr0 { return ["set", name, expr]; }
+
+Expr0
+    = head:Expr1 tail:((_"?"/_"^") Expr1)* {
+        var h = head;
+        for (var i = 0; i < tail.length; i ++)
+            h = [tail[i][0][1], h, tail[i][1]];
+        return h;
+    }
+
+Expr1
+    = head:Expr2 tail:(_"+" Expr2)* {
+        var h = head;
+        for (var i = 0; i < tail.length; i ++)
+            h = ["+", h, tail[i][2]];
+        return h;
+    }
+
+Expr2
+    = head:Expr3 tail:(_"*" Expr3)* {
+        var h = head;
+        for (var i = 0; i < tail.length; i ++)
+            h = ["*", h, tail[i][2]];
+       return h;
+    }
+
+Expr3
+    = head:Expr4 tail:((_"<<"/_">>") Expr4)* {
+        var h = head;
+        for (var i = 0; i < tail.length; i ++)
+            h = [tail[i][0][1], h, tail[i][1]];
+       return h;
+    }
+
+Expr4
+    = x:Expr5 { return x; }
+    / _"-" x:Expr5 { return ["neg", x]; }
+
+Expr5
+    = _"(" x:Expr0 _")" { return x; }
+    / _"s" _"(" x:Expr0 _")" { return ["sigmoid", x]; }
+    / _"in" _"(" x:Integer _")" { return ["in", x]; }
+    / x:Ident { return ["ref", x]; }
+    / x:Number { return ["const", x];}
+
+Number "number"
+    = _ digits:$([0-9.]+) { return digits; }
+
+Integer "integer"
+    = _ digits:$([0-9]+) { return parseInt(digits); }
+
+Ident "identifier"
+    = _ id:$([a-zA-Z] [a-zA-Z0-9_]*) {
+        return id;
+    }
+
+_ "whitespace"
+    = [ \t\n\r]*
+
+// vim: sw=4 ts=4 et
